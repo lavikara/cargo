32b9983a1d84fc9f7bbcc2a8644b3a558928c4e6
lavikara <you@example.com>
2024-03-06 09:50:13 +0100

    highlight team size when selected

--- src/layout/auth/Kyc.vue
+++ src/layout/auth/Kyc.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="kyc-layout">
     <div class="tw-pb-20 tw-mt-16">
-      <h3 class="tw-max-w-xs tw-font-semibold tw-text-xl tw-mb-4">
+      <h3
+        class="tw-max-w-xs tw-font-semibold tw-text-xl lg:tw-text-2xl tw-mb-4"
+      >
         You’re almost there!
       </h3>
       <p class="tw-text-sm tw-mb-8">
@@ -49,22 +51,39 @@
           </label>
           <div class="tw-flex tw-justify-between tw-mt-1">
             <div
-              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-py-3"
+              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-transition-all tw-duration-300 tw-py-3"
+              :class="{
+                'tw-text-white tw-bg-blue !tw-border-blue': teamSize === 5,
+              }"
+              @click="setTeamSize(5)"
             >
               1 to 5
             </div>
             <div
-              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-py-3"
+              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-transition-all tw-duration-300 tw-py-3"
+              :class="{
+                'tw-text-white tw-bg-blue !tw-border-blue': teamSize === 10,
+              }"
+              @click="setTeamSize(10)"
             >
               6 to 10
             </div>
             <div
-              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-py-3"
+              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-transition-all tw-duration-300 tw-py-3"
+              :class="{
+                'tw-text-white tw-bg-blue !tw-border-blue': teamSize === 30,
+              }"
+              @click="setTeamSize(30)"
             >
               11 to 30
             </div>
             <div
-              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-py-3"
+              class="tw-w-[6rem] tw-flex tw-justify-center tw-text-xs tw-border tw-border-gray tw-rounded-2xl tw-cursor-pointer hover:tw-border-blue tw-transition-all tw-duration-300 tw-py-3"
+              :class="{
+                'tw-text-white tw-bg-blue !tw-border-blue':
+                  teamSize === 'above 30',
+              }"
+              @click="setTeamSize('above 30')"
             >
               Above 30
             </div>
@@ -79,19 +98,19 @@
             id="location"
             :showLabel="true"
             :leftSide="true"
-            :list="roles"
+            :list="stateList"
             :disabled="baseStore.btnLoading"
             @set="setState"
           />
           <SelectInput
             class="tw-col-start-4 tw-mt-6"
+            label="Country"
             defaltOption="Country"
             name="role"
             id="role"
             bgColor="gray"
-            :showLabel="true"
             :rightSide="true"
-            :list="roles"
+            :list="countryList"
             :disabled="baseStore.btnLoading"
             @set="setCountry"
           />
@@ -110,9 +129,10 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 import { useBaseStore } from "@/stores/baseStore.js";
+import countryRegionData from "@/utils/js/countryRegionData.js";
 import TextInput from "@/components/general/TextInput.vue";
 import SelectInput from "@/components/general/SelectInput.vue";
 import Btn from "@/components/general/Btn.vue";
@@ -122,6 +142,27 @@ const baseStore = useBaseStore();
 const validResults = ref([{ email: false, password: false }]);
 const formValid = ref(false);
 const payload = ref({});
+const teamSize = ref("");
+
+const countryList = computed(() => {
+  let list = [];
+  countryRegionData.map((countryObj) => {
+    list.push(countryObj.countryName);
+  });
+  return list;
+});
+
+const stateList = computed(() => {
+  let list = [];
+  countryRegionData.map((countryObj) => {
+    if (countryObj.countryName === payload.value.country) {
+      countryObj.regions.map((region) => {
+        list.push(region.name);
+      });
+    }
+  });
+  return list;
+});
 
 const addKyc = () => {
   setTimeout(async () => {
@@ -146,7 +187,11 @@ const setState = (text) => {
 };
 
 const setCountry = (text) => {
-  payload.value.password = text;
+  payload.value.country = text;
+};
+
+const setTeamSize = (size) => {
+  teamSize.value = size;
 };
 
 const updateValidResult = (payload) => {
